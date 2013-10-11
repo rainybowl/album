2d92a80787c1f6342f44c4c2d6eeecbc0564fef8
rainybowl <rainybowl@users.noreply.github.com>
2013-10-11 19:39:07 +0400

    Create components.js

---
+++ components.js
@@ -0,0 +1,21 @@
+var settings = {
+	'lectures': {
+		template: 'smallList',
+		path: '#lectures ul'		
+	},
+	
+	'all_lectures': {
+		template: 'smallList',
+		path: '#okno'		
+	},
+					
+	'lectors': {
+		template: 'smallList',
+		path: '#lectors'	
+	},
+	
+	'lector': {
+		template: 'lector',
+		path: '#okno'	
+	}		
+}
